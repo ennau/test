de46c9545e89ea2482cd20a538a2f55117faf166
ennau <ennau1@ukr.net>
2016-02-29 22:39:22 +0200

    Update hello.py

--- web/hello.py
+++ web/hello.py
@@ -1,21 +1,14 @@
-from cgi import parse_qs, escape
+from cgi import parse_qs
 
-def wsgi_application(environ, start_response):
-	parameters = parse_qs(environ.get('QUERY_STRING', ''))
-	if 'subject' in parameters:
-		subject = escape(parameters['subject'][0])
-	else:
-		subject = 'Test' 
-	status = '200 OK'
-	headers = [
-		('Content-Type', 'text/plain')
-	]
-	#body = 'Hello, world!'
-	start_response(status, headers) 
-	return [ '''HELLO %(subject)s 
-	Hello %(subject)s!
+def wsgi_app(environ, start_response):
+    start_response('200 OK', [('Content-Type', 'text/plain')])
+    query = environ.get('QUERY_STRING', '')
+    for param in querys.split('&'):
+        yield param + '\n'
+        
+        
 
-''' % {'subject': subject}]
+
 
 
 
