e5286a5fa8e1c0ef328671607e35b2a2f140be60
ennau <ennau1@ukr.net>
2016-02-29 21:54:36 +0200

    Create init.sh

---
+++ init.sh
@@ -0,0 +1,6 @@
+sudo rm -rf /etc/nginx/sitest-enabled/default                                   
+sudo ln -sf /home/box/web/etc/nginx_test.conf /etc/nginx/sites-enabled/nginx_test.conf     
+sudo /etc/init.d/nginx restart                                                  
+                                                                                
+sudo ln -s /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/gunicorn_test.conf            
+sudo /etc/init.d/gunicorn restart         
