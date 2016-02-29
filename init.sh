2692e0b3c55fbc08ddd358a815ace9f24d1942ed
ennau <ennau1@ukr.net>
2016-02-29 22:44:24 +0200

    Update init.sh

--- init.sh
+++ init.sh
@@ -2,5 +2,6 @@ sudo rm -rf /etc/nginx/sitest-enabled/default
 sudo ln -sf /home/box/web/etc/nginx_test.conf /etc/nginx/sites-enabled/nginx_test.conf     
 sudo /etc/init.d/nginx restart                                                  
 
-sudo ln -s /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/hello                                                                                
+sudo rm -rf /etc/gunicorn.d/heelo
+sudo ln -s /home/box/web/gunicorn.conf /etc/gunicorn.d/hello                                                                                
 sudo /etc/init.d/gunicorn restart         
